42dcc380b62a435e2daff57082d6e540a32ab466
Sid Jr <sidalbertok@gmail.com>
2026-03-19 16:08:43 -0500

    Create ponteiro_c.c

---
+++ C/Memoria Ponteiro/ponteiro_c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+   char *str = "salve gurizada";
+   printf("String Inteira: %s\n",str);
+   printf("String Inteira: %c\n",str);
+   printf("String Inteira: %c\n",(str+1));
+   return 0;
+   
+}
