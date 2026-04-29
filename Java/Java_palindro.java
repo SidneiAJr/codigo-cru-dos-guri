81ba1c2ab11b94dd9d883036a90c4e1b8dcf469a
Sid Jr <sidalbertok@gmail.com>
2026-04-29 15:39:53 -0300

    Create Java_palindro.java

---
+++ Java/Java_palindro.java
@@ -0,0 +1,33 @@
+
+package teste;
+
+
+public class Teste {
+
+   
+    public static void main(String[] args) {
+       testaPalavra t = new testaPalavra("Teste");
+       t.testar();
+    }
+}
+
+class testaPalavra{
+    private String palavra;
+
+    public testaPalavra(String palavra) {
+        this.palavra = palavra;
+    }
+    
+    public void testar(){
+        String invertida = new StringBuilder(palavra).reverse().toString();
+        if(palavra.equalsIgnoreCase(invertida)){
+            System.out.println("Palindro!");
+        }else{
+            System.out.println("Nao e Palindro!");
+        }
+        
+        
+    }
+    
+    
+}
