89bc4577ed35dcbff995f5a0577e966cbc2230af
Sid Jr <sidalbertok@gmail.com>
2026-05-09 16:38:16 -0300

    Create Atividade2.js

---
+++ Javascript/Teste Logica/Atividade2.js
@@ -0,0 +1,17 @@
+// Objeto de Pessoas
+let pessoa = [{
+    nome:"pedro",idade: 11,
+    nome: "Juliana", idade: 20,
+    nome: "Jorge",idade: 15
+}];
+
+//Usando Filtrer
+let maiores = pessoa.filter(p=>p.idade>=18);
+console.log("Maiores de idade:",maiores)
+
+// Usando map
+let resultado = pessoa.map(pessoa=>{
+    return `${pessoa.nome} é ${pessoa.idade >=18 ? "maior": "menor"} de idade`;
+})
+
+console.log(resultado)
