bce7143a0ea7904446ae5aebc4887025556bc0d9
Sid <sidalbertok@gmail.com>
2026-09-06 13:50:24 -0300

    feat: Adiciona operadores novos

---
+++ Javascript/Operadores Logicos e Logica/Operadores/indexof.js
@@ -0,0 +1,13 @@
+const teste = ["a","b","c"]
+
+const posicao1 = teste.indexOf("a")
+const posicao2 = teste.indexOf("b")
+const posicao3 = teste.indexOf("c")
+
+console.log(`Posição no array: ${posicao1}`)
+console.log(`Posição no array: ${posicao2}`)
+console.log(`Posição no array: ${posicao3}`)
+
+console.log(typeof posicao1)
+console.log(typeof posicao2)
+console.log(typeof posicao3)
